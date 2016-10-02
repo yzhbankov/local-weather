961780f9583a5bc8b7189b0f769a49baae81515d
Iaroslav Zhbankov <yzhbankov2@gmail.com>
2016-10-02 23:26:21 +0300

    transition from F to C

--- app.js
+++ app.js
@@ -8,14 +8,28 @@ $.ajax({
     url: "https://api.darksky.net/forecast/5dd4dc7ef0ce4021dd39c00c60bf98f4/" + YMaps.location.latitude.toString() + "," + YMaps.location.longitude.toString(),
     dataType: 'jsonp',
     success: function (response) {
-        $(".temperature").html(((response.currently.temperature - 32) * 5 / 9).toFixed(1) + ", C");
+        $(".temperature").html(response.currently.temperature.toFixed(1));
+        $(".temperatureSwitcher").html("F");
         $(".sky").html(response.currently.icon);
         var windB = response.currently.windBearing;
         $(".wind").html((response.currently.windSpeed * 1609.34 / 3600).toFixed(1) + " m/sec, " + windDirection(windB));
     }
 });
 
-function windDirection(windB){
+var temperatureSwitcher = document.getElementsByClassName("temperatureSwitcher");
+
+temperatureSwitcher[0].addEventListener("click", function () {
+    var temperature = Number($(".temperature").text());
+    if ($(".temperatureSwitcher").text() == "F") {
+        $(".temperatureSwitcher").html("C");
+        $(".temperature").html(((temperature - 32) * 5 / 9).toFixed(1));
+    } else {
+        $(".temperatureSwitcher").html("F");
+        $(".temperature").html((temperature * 1.8 + 32).toFixed(1));
+    }
+});
+
+function windDirection(windB) {
     var windDirection;
     if ((windB == 0) || (windB == 360)) {
         windDirection = "N"
